d90421a6cc1a18c36a6162326ef3444d4cd53f6d
kknsn <hrmc5@yahoo.co.jp>
2021-03-22 01:38:31 +0900

    showアクションのルーティング設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,10 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def all_index
     @books =Book.all.order("created_at DESC")
   end
